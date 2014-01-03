3729f658c8de82a6bde2506743601a92078ccbdc
Ivan Pedrazas <ipedrazas@gmail.com>
2014-01-03 23:03:21 +0000

    item model and test added

--- blibbapi/models/blibb.py
+++ blibbapi/models/blibb.py
@@ -23,3 +23,25 @@ class Collection(Base):
                 'name': self.name,
                 'owner': self.owner
                 }
+
+
+class Item(Base):
+
+    __tablename__ = 'blibb_item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), unique=True)
+    owner = Column(String(200))
+
+    def __init__(self, name, owner):
+        self.name = name
+        self.owner = owner
+
+    def __repr__(self):
+        return '<Item: %s, owner: %s>' % (self.name, self.owner)
+
+    def _serialise(self):
+        return {
+                'name': self.name,
+                'owner': self.owner
+                }
--- blibbapi/tests/models/test_blibb.py
+++ blibbapi/tests/models/test_blibb.py
@@ -10,7 +10,7 @@ parentpath = abspath(join(dirname(__file__), '../../..'))
 sys.path.append(parentpath)
 
 from blibbapi.db import get_engine, init_db, get_db_session, Base
-from blibbapi.models.blibb import Collection
+from blibbapi.models.blibb import Collection, Item
 
 
 class TestCollectionModel(unittest.TestCase):
@@ -36,10 +36,34 @@ class TestCollectionModel(unittest.TestCase):
 
         self.assertEquals(len(results), 1)
         self.assertEquals(results[0].__repr__(), u'<Collection: First Collection, owner: Ivan>')
-
         self.assertEquals(results[0]._serialise(), {'name': 'First Collection', 'owner': 'Ivan'})
 
 
+
+class TestItemModel(unittest.TestCase):
+
+    def setUp(self):
+        # self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        test_sql = 'sqlite:///test_db.sql'
+        self.engine = get_engine(test_sql)
+        self.db = init_db(self.engine, Base)
+        self.db_session = get_db_session(self.engine)
+
+    def tearDown(self):
+        self.db_session.remove()
+        os.unlink('test_db.sql')
+
+    def test_basic(self):
+        item = Item('First Item', 'Ivan')
+        self.db_session.add(item)
+        self.db_session.commit()
+
+        results = self.db_session.query(Item).all()
+        print str(results)
+
+        self.assertEquals(len(results), 1)
+        self.assertEquals(results[0].__repr__(), u'<Item: First Item, owner: Ivan>')
+        self.assertEquals(results[0]._serialise(), {'name': 'First Item', 'owner': 'Ivan'})
 
 
 
